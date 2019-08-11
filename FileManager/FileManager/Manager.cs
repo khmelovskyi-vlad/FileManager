45b5c20d51d2536e782bb0199a7474205d1e8502
vladhmelevskiy1234765 <vladhmelevskiy1234765@gmail.com>
2019-08-11 19:20:41 +0300

    File Editor

--- FileManager/FileManager/Manager.cs
+++ FileManager/FileManager/Manager.cs
@@ -13,20 +13,26 @@ namespace FileManager
         {
         }
         private string adressName { get; set; }
+        StringBuilder textBuilderEnd;
+        StringBuilder allTextBuilder;
+        StringBuilder textBuilderFirst;
+        int topPosition;
+        int leftPosition;
+        string[] fileLines;
         private bool AdressHaving()
         {
             return File.Exists(adressName);
         }
         public void FileManager()
         {
-            FirstDisk();
+            SelectDisk();
             while (true)
             {
                 Console.WriteLine("If you want to select a different folder, click Enter");
                 var key = Console.ReadKey(true);
                 if(key.Key == ConsoleKey.Enter)
                 {
-                    InFolder();
+                    InFolderOrFile();
                 }
                 else if(key.Key == ConsoleKey.Backspace)
                 {
@@ -42,16 +48,15 @@ namespace FileManager
         {
             try
             {
-                var k = Path.GetDirectoryName(adressName);
-                adressName = Path.GetDirectoryName(k);
-                OutPutFiles();
+                adressName = Path.GetDirectoryName(adressName);
+                OutPutFoldersAndFiles();
             }
             catch (ArgumentNullException)
             {
-                FirstDisk();
+                SelectDisk();
             }
         }
-        private void InFolder()
+        private void InFolderOrFile()
         {
             Console.WriteLine("Select a folder");
             while (true)
@@ -60,9 +65,9 @@ namespace FileManager
                 try
                 {
                     var fileName = Console.ReadLine();
-                    var adress = $"{fileName}\\";
-                    adressName = $"{adressName}{adress}";
-                    OutPutFiles();
+                        var adress = $"{fileName}\\";
+                        adressName = $"{adressName}{adress}";
+                        OutPutFoldersAndFiles();
                     break;
                 }
                 catch (DirectoryNotFoundException ex)
@@ -70,45 +75,345 @@ namespace FileManager
                     adressName = saveAdress;
                     Console.WriteLine($"Bed input {ex}, try again");
                 }
+                catch(IOException)
+                {
+                    adressName = adressName.Substring(0, adressName.Length - 1);
+                    EditingFile();
+                    break;
+                }
             }
         }
-        private void FirstDisk()
+        private void SelectDisk()
         {
+            Console.Write("You have disks: ");
+            var allDisks = Directory.GetLogicalDrives();
+            foreach (var disk in allDisks)
+            {
+                Console.Write($"{disk}, ");
+            }
+            Console.WriteLine();
             Console.WriteLine("Select a disc");
             while (true)
             {
-                Console.WriteLine("If you want 'C' disc click c, if 'D' disc click d, if you want to exit, click Escape");
-                var key = Console.ReadKey(true);
-                if (key.Key == ConsoleKey.C)
+                Console.WriteLine("Write name your disk");
+                var line = Console.ReadLine();
+                adressName = $"{line}:\\";
+                var flag = false;
+                foreach(var disk in allDisks)
                 {
-                    adressName = "C:\\";
-                    OutPutFiles();
+                    if(disk == adressName)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    OutPutFoldersAndFiles();
                     break;
                 }
-                else if (key.Key == ConsoleKey.D)
-                {
-                    adressName = "D:\\";
-                    OutPutFiles();
-                    break;
-                }
-                else if (key.Key == ConsoleKey.Escape)
-                {
-                    throw new OperationCanceledException();
-                }
-                Console.WriteLine("Bed input");
             }
         }
-        private void OutPutFiles()
+        private void OutPutFoldersAndFiles()
         {
-            var allDocument = Directory.GetFiles(adressName);
-            var allFolder = Directory.GetDirectories(adressName);
-            var allFiles = allFolder.Concat(allDocument);
-            Console.Write("In this folder are: ");
-            foreach (var file in allFiles)
+            var allDirectories = Directory.GetDirectories(adressName);
+            var allFiles = Directory.GetFiles(adressName);
+            //var allFiles = allDirectories.Concat(allDocument);
+            if (allDirectories.Length != 0)
             {
-                Console.Write($"{Path.GetFileName(file)}, ");
+                Console.WriteLine();
+                Console.WriteLine("This directory has following directories: ");
+                foreach (var directory in allDirectories)
+                {
+                    Console.WriteLine($"{Path.GetFileName(directory)}, ");
+                }
+            }
+            if (allFiles.Length != 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("This directory has following files: ");
+                foreach (var file in allFiles)
+                {
+                    Console.WriteLine($"{Path.GetFileName(file)}, ");
+                }
             }
             Console.WriteLine();
+        }
+        private void EditingFile()
+        {
+            var undestand = UnderstandingQuestions();
+            if (undestand.Key == ConsoleKey.Enter)
+            {
+            Console.Clear();
+            fileLines = File.ReadAllLines(adressName);
+            WriteLines(fileLines);
+            topPosition = fileLines.Length - 1;
+            textBuilderFirst = new StringBuilder(fileLines[topPosition]);
+            textBuilderEnd = new StringBuilder();
+            allTextBuilder = new StringBuilder();
+            leftPosition = textBuilderFirst.Length;
+                Console.SetCursorPosition(leftPosition, topPosition);
+                while (true)
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.LeftArrow)
+                    {
+                        LeftArrow();
+                    }
+                    else if (key.Key == ConsoleKey.RightArrow)
+                    {
+                        RightArrow();
+                    }
+                    else if (key.Key == ConsoleKey.UpArrow)
+                    {
+                        topPosition--;
+                    }
+                    else if (key.Key == ConsoleKey.DownArrow)
+                    {
+                        topPosition++;
+                    }
+                    else if (key.Key == ConsoleKey.Backspace)
+                    {
+                        Backspace();
+                    }
+                    else if (key.Key == ConsoleKey.Delete)
+                    {
+                        Delete();
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        Enter();
+                    }
+                    else if (key.Key == ConsoleKey.Escape)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        ElseClick(key);
+                    }
+                    if (key.Key == ConsoleKey.UpArrow || key.Key == ConsoleKey.DownArrow)
+                    {
+                        UpOrDownArrow();
+                    }
+                    Console.SetCursorPosition(leftPosition, topPosition);
+                }
+            SavingFile(fileLines);
+            }
+            BackFolder();
+        }
+        private ConsoleKeyInfo UnderstandingQuestions()
+        {
+            Console.WriteLine("If you want to edit the text click Enter, else click another key");
+            Console.WriteLine("If you press enter, only the text of your file will be displayed on the console, at the end of editing press Escape");
+            var key = Console.ReadKey(true);
+            return key;
+        }
+        private void SavingFile (string[] fileLine)
+        {
+            Console.WriteLine();
+            Console.WriteLine("If you want to save this file, click Enter, else, enouther click");
+            var key = Console.ReadKey(true);
+            Console.Clear();
+            if (key.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine("File was saving");
+                File.WriteAllLines(adressName, fileLine, Encoding.Default);
+            }
+        }
+        private static void WriteLines(string[] fileStrings)
+        {
+            foreach (var fileString in fileStrings)
+            {
+                Console.WriteLine(fileString);
+            }
+        }
+        private void LeftArrow()
+        {
+            if (leftPosition != 0)
+            {
+                textBuilderEnd.Insert(0, textBuilderFirst[textBuilderFirst.Length - 1]);
+                textBuilderFirst.Remove(textBuilderFirst.Length - 1, 1);
+                leftPosition--;
+            }
+            else
+            {
+                if (leftPosition == 0 && topPosition == 0)
+                {
+                    leftPosition = 0;
+                    return;
+                }
+                topPosition--;
+                textBuilderFirst = new StringBuilder(fileLines[topPosition]);
+                leftPosition = textBuilderFirst.Length;
+                textBuilderEnd.Clear();
+            }
+            return;
+        }
+        private void RightArrow()
+        {
+            var lengthFiletLine = fileLines[topPosition].Length;
+            if (leftPosition != lengthFiletLine)
+            {
+                textBuilderFirst.Append(textBuilderEnd[0]);
+                textBuilderEnd.Remove(0, 1);
+                leftPosition++;
+            }
+            else
+            {
+                if (leftPosition == lengthFiletLine && topPosition == fileLines.Length - 1)
+                {
+                    leftPosition = textBuilderFirst.Length;
+                    return;
+                }
+                topPosition++;
+                textBuilderFirst.Clear();
+                leftPosition = 0;
+                textBuilderEnd = new StringBuilder(fileLines[topPosition]);
+            }
+            return;
+        }
+        private void UpOrDownArrow()
+        {
+            if (topPosition == fileLines.Length)
+            {
+                topPosition = fileLines.Length - 1;
+                return;
+            }
+            if (topPosition < 0)
+            {
+                topPosition = 0;
+                return;
+            }
+            textBuilderFirst = new StringBuilder(fileLines[topPosition]);
+            var countEndBuilder = textBuilderFirst.Length - leftPosition;
+            textBuilderEnd.Clear();
+            if (countEndBuilder >= 0)
+            {
+                textBuilderEnd.Append(Convert.ToString(textBuilderFirst), leftPosition, countEndBuilder);
+                textBuilderFirst.Remove(leftPosition, countEndBuilder);
+            }
+            else
+            {
+                leftPosition = textBuilderFirst.Length;
+            }
+            return;
+        }
+        private void Backspace()
+        {
+            allTextBuilder.Clear();
+            Console.Clear();
+            if (textBuilderFirst.Length != 0)
+            {
+                textBuilderFirst.Remove(textBuilderFirst.Length - 1, 1);
+            }
+            else
+            {
+                topPosition--;
+                if (topPosition >= 0)
+                {
+                    textBuilderFirst = new StringBuilder(fileLines[topPosition]);
+                }
+                else
+                {
+                    topPosition++;
+                    WriteLines(fileLines);
+                    return;
+                }
+                DeleteBackspaceWriter(false);
+                fileLines = SmallArrayString(fileLines, topPosition);
+                WriteLines(fileLines);
+                leftPosition = textBuilderFirst.Length;
+                return;
+            }
+            DeleteBackspaceWriter(true);
+            leftPosition--;
+            return;
+        }
+        private void DeleteBackspaceWriter(bool withWriteLines)
+        {
+            allTextBuilder.Append(textBuilderFirst);
+            allTextBuilder.Append(textBuilderEnd);
+            fileLines[topPosition] = Convert.ToString(allTextBuilder);
+            if (withWriteLines)
+            {
+                WriteLines(fileLines);
+            }
+        }
+        private void Delete()
+        {
+            allTextBuilder.Clear();
+            Console.Clear();
+            if (textBuilderEnd.Length != 0)
+            {
+                textBuilderEnd.Remove(0, 1);
+            }
+            else
+            {
+                if (topPosition < fileLines.Length - 1)
+                {
+                    textBuilderEnd = new StringBuilder(fileLines[topPosition + 1]);
+                }
+                else
+                {
+                    WriteLines(fileLines);
+                    return;
+                }
+                DeleteBackspaceWriter(false);
+                fileLines = SmallArrayString(fileLines, topPosition);
+                WriteLines(fileLines);
+                return;
+            }
+            DeleteBackspaceWriter(true);
+            return;
+        }
+        private void Enter()
+        {
+            Console.Clear();
+            fileLines[topPosition] = Convert.ToString(textBuilderFirst);
+            textBuilderFirst.Clear();
+            topPosition++;
+            leftPosition = 0;
+            fileLines = BigArrayString(fileLines, topPosition);
+            fileLines[topPosition] = Convert.ToString(textBuilderEnd);
+            WriteLines(fileLines);
+            return;
+        }
+        private void ElseClick(ConsoleKeyInfo key)
+        {
+            allTextBuilder.Clear();
+            Console.Clear();
+            textBuilderFirst.Append(key.KeyChar);
+            DeleteBackspaceWriter(true);
+            leftPosition++;
+            return;
+        }
+        private static string[] BigArrayString(string[] fileText, int topPosition)
+        {
+            string[] bigArrayString = new string[fileText.Length + 1];
+            for (int i = 0, j = 0; i < bigArrayString.Length; i++, j++)
+            {
+                if (i == topPosition)
+                {
+                    j--;
+                    continue;
+                }
+                bigArrayString[i] = fileText[j];
+            }
+            return bigArrayString;
+        }
+        private static string[] SmallArrayString(string[] fileText, int topPosition)
+        {
+            string[] smallArrayString = new string[fileText.Length - 1];
+            for (int i = 0, j = 0; i < smallArrayString.Length; i++, j++)
+            {
+                if (i == topPosition + 1)
+                {
+                    j++;
+                }
+                smallArrayString[i] = fileText[j];
+            }
+            return smallArrayString;
         }
     }
 }
--- FileManager/FileManager/Program.cs
+++ FileManager/FileManager/Program.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using IFilterTextReader;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using Word = Microsoft.Office.Interop.Word;
+//using System.Reflection;
 
 namespace FileManager
 {
@@ -11,6 +14,28 @@ namespace FileManager
     {
         static void Main(string[] args)
         {
+            //Word._Application application;
+            //Word._Document document;
+            //Object missingObj = System.Reflection.Missing.Value;
+            //Object trueObj = true;
+            //Object falseObj = false;
+            //var w = "C:\\GIT\\w.txt";
+            //var q = "C:\\GIT\\q.docx";
+            //var fileStrings = File.ReadAllLines(w, Encoding.Default);
+            //foreach (var fileString in fileStrings)
+            //{
+            //    Console.WriteLine(fileString);
+            //}
+            //Console.WriteLine();
+            //var s = File.ReadAllText(w, Encoding.GetEncoding(10017));
+            //StreamReader st = new StreamReader(w, Encoding.Default);
+            //Console.WriteLine(st.ReadToEnd());
+            //var qnew = new FilterReader(q);
+            //q = qnew.ReadToEnd();
+            //Console.WriteLine(q);
+            //application = new Word.Application();
+            //document = application.Documents.Add(ref q, ref missingObj, ref missingObj, ref missingObj);
+            //Console.WriteLine(document);
             Manager manager = new Manager();
             manager.FileManager();
             Console.ReadKey();
